131ae289477eddebbf1b6946fd1f177ca58d59cc
Júlia Fernandes Alves <juliafealves@gmail.com>
2023-03-03 20:57:25 -0300

    Created the function to read file.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import chalk from "chalk";
+import fs from "fs";
 
-console.log(chalk.blue.bold("Olá!"))
+function handleError(error) {
+  throw new Error(chalk.red(error.code, "Erro ao ler arquivo!"));
+}
+
+async function readFile(path) {
+  try {
+    const content = await fs.promises.readFile(path, "utf-8");
+    console.log(chalk.green(content));
+  } catch (error) {
+    handleError(error);
+  } finally {
+    console.log(chalk.yellow("Operação concluída!"));
+  }
+}
+
+readFile("./files/text.md");
